f4a3b9398c69395133adb71e4d7777a8971a3003
Tech Penguin <131509539+Ryan-Rudd@users.noreply.github.com>
2023-05-30 16:23:33 -0400

    added routing

--- Zenith/Web.py
+++ Zenith/Web.py
@@ -1,18 +1,33 @@
 import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 
 class Server(BaseHTTPRequestHandler):
     HEADER_VALUE = "text/html"
     HEADER_TYPE = "Content-type"
-    
+    ROUTES = []
+        
     def do_GET(self):
         self.send_response(200)
         self.send_header(self.HEADER_TYPE, self.HEADER_VALUE)
         self.end_headers()
-        self.wfile.write(bytes("<h1>Zenith Server</h1>", "utf-8"))
+
+        for route, response in self.ROUTES:
+            if self.path == f"/{route}":
+                self.wfile.write(response.encode("utf-8"))
+                return
+
+        self.wfile.write(b"<h1>Zenith Server</h1>")
+    
+    def getPath(self, routeName):
+        if self.path == f"/{routeName}":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("<b> Hello World !</b>"
+                             + "<br><br>Current time: " + str(datetime.datetime.now()))
         
 class WApp(Server):
+
     def __init__(self) -> None:
         self.port = 3000
         self.host = 'localhost'
@@ -20,25 +35,27 @@ class WApp(Server):
         self.static_url_path = "static"
         self.name = "Zenith App"
         self.logging = True
+        self.webServer = HTTPServer((self.host, self.port), Server)
     
-    class set:
-        def headers(TYPE, VALUE):
-            Server.do_GET()
-            
+    def get(self, route, response):
+        Server.ROUTES.append((route, response))
+        
     def serve(self):
         current_time = datetime.datetime.now()
         time_stamp = current_time.timestamp()
-
-        webServer = HTTPServer((self.host, self.port), Server)
         print(f"Starting server at http://{self.host}:{self.port}")
         if self.devServer:
             print(f"\t• Running as development server")
         print(f"\t• Started {datetime.datetime.fromtimestamp(time_stamp)}\n")
         try:
-            webServer.serve_forever()
-        except  KeyboardInterrupt:
+            self.webServer.serve_forever()
+        except KeyboardInterrupt:
             pass
-        webServer.server_close()
+        self.webServer.server_close()
         print("Server stopped.")
 
 
+app = WApp()
+
+app.get("/hello", "hello")
+app.serve()
