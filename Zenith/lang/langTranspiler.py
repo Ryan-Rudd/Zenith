28d0ef84c977fd858e1d09f16497582fb77e4d81
Tech Penguin <131509539+Ryan-Rudd@users.noreply.github.com>
2023-05-30 21:49:16 -0400

    fixed transpiler to transpile attributes not only functions

--- Zenith/lang/langTranspiler.py
+++ Zenith/lang/langTranspiler.py
@@ -1,75 +1,56 @@
 def transpile_to_html(node):
-    transpile_functions = {
-        "Container": transpile_container,
-        "Header": transpile_header,
-        "Block": transpile_block,
-        "Button": transpile_button
-    }
+    result = ""
 
-    if node.tag_name in transpile_functions:
-        transpile_func = transpile_functions[node.tag_name]
-        return transpile_func(node)
-    
-    # Default transpilation for unknown tags
-    return transpile_unknown(node)
+    if node.tag_name == "Container":
+        result += "<div"
 
+        for attr_name, attr_value in node.attributes.items():
+            attr_name = transpile_attribute_name(attr_name)
+            result += f' {attr_name}="{attr_value}"'
 
-def transpile_container(node):
-    result = "<div"
+        result += ">"
 
-    for attr_name, attr_value in node.attributes.items():
-        result += f' {attr_name}="{attr_value}"'
+        for child_node in node.children:
+            result += transpile_to_html(child_node)
 
-    result += ">"
+        result += "</div>"
 
-    for child_node in node.children:
-        result += transpile_to_html(child_node)
+    elif node.tag_name == "Header":
+        result += "<h1"
 
-    result += "</div>"
+        for attr_name, attr_value in node.attributes.items():
+            attr_name = transpile_attribute_name(attr_name)
+            result += f' {attr_name}="{attr_value}"'
+
+        result += f">{node.content}</h1>"
+
+    elif node.tag_name == "Block":
+        result += "<div"
+
+        for attr_name, attr_value in node.attributes.items():
+            attr_name = transpile_attribute_name(attr_name)
+            result += f' {attr_name}="{attr_value}"'
+
+        result += ">"
+
+        for child_node in node.children:
+            result += transpile_to_html(child_node)
+
+        result += "</div>"
+
+    elif node.tag_name == "Button":
+        result += "<button"
+
+        for attr_name, attr_value in node.attributes.items():
+            attr_name = transpile_attribute_name(attr_name)
+            result += f' {attr_name}="{attr_value}"'
+
+        result += f">{node.content}</button>"
+
     return result
 
 
-def transpile_header(node):
-    result = "<h1"
-
-    for attr_name, attr_value in node.attributes.items():
-        result += f' {attr_name}="{attr_value}"'
-
-    result += f">{node.content}</h1>"
-    return result
-
-
-def transpile_block(node):
-    result = "<div"
-
-    for attr_name, attr_value in node.attributes.items():
-        result += f' {attr_name}="{attr_value}"'
-
-    result += ">"
-
-    for child_node in node.children:
-        result += transpile_to_html(child_node)
-
-    result += "</div>"
-    return result
-
-
-def transpile_button(node):
-    result = "<button"
-
-    for attr_name, attr_value in node.attributes.items():
-        result += f' {attr_name}="{attr_value}"'
-
-    result += f">{node.content}</button>"
-    return result
-
-
-def transpile_unknown(node):
-    # Default transpilation for unknown tags
-    result = f"<{node.tag_name}"
-
-    for attr_name, attr_value in node.attributes.items():
-        result += f' {attr_name}="{attr_value}"'
-
-    result += f">{node.content}</{node.tag_name}>"
-    return result
+def transpile_attribute_name(attribute_name):
+    if attribute_name == "idName":
+        return "id"
+    return attribute_name
--- examples/server.py
+++ examples/server.py
@@ -5,12 +5,13 @@ app = Zenith.WApp()
 homePageContent = """
     <Container>
         <Header idName="header">Hello world from Zenith</Header>
+        <Button idName="test-button">Press Me!</Button>
     </Container>
     """
     
 builder = Zenith.Builder({"home": homePageContent})
 built = builder.Build()
-print(built)
+
 for route in builder.getRoutes():
     app.register_route(f'{route}', built[route])
     
